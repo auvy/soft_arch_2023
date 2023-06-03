36dec7a8b0172c7b9b30c8fc53e8a141306d52d0
auvy <datruegvoltage@gmail.com>
2023-06-03 19:48:51 +0300

    fix: check empty connection

--- 1-layered/bl.py
+++ 1-layered/bl.py
@@ -1,7 +1,16 @@
 class BL:
+  def __init__(self):
+    print("bl created")
+  
   def __init__(self, dl_conn):
     print("bl created")
     self.dl = dl_conn
+    
+  def conn(self, dl_conn):
+    self.dl = dl_conn
   
   def get_data(self):
-    return self.dl.get_all()
+    if hasattr(self, "dl"):
+      return self.dl.get_all()
+    else:
+      return False
--- 1-layered/dal.py
+++ 1-layered/dal.py
@@ -2,20 +2,19 @@ class DAL:
   def __init__(self):
     print("ok")
   
-  def __init__(self, connection):
-    print("connected idk")
-    self.db = connection
-  
   def connect(self, connection):
     # pretend that connection is not a list lol
     print("reconnected")
     self.db = connection
   
   def create(self, data):
-    self.db.append(data)
+    if hasattr(self, "db"):
+      self.db.append(data)
+    else:
+      return False
   
   def get_all(self):
-    if self.db:
+    if hasattr(self, "db"):
       return self.db
     return False
 
